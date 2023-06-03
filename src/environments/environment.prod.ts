bdd7a1509e1b4c6e6a853549fd8eb024e57fb5f4
Omar Gonzalez <omargonzalez9911@yahoo.com>
2023-06-03 11:13:35 -0500

    Se adjunta cambio de ip de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
-    urlApi: 'http://189.240.200.7:8028/api',
+    //urlApi: 'http://189.240.200.7:8028/api',
     recaptcha: {
         siteKey: '6LfWQeQjAAAAALCKcqwYte_HzBS1DJ8hM8XIKybo',
     },
+    urlApi: 'http://187.175.225.67:8028/api'
 };
