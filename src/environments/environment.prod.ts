0d30ebe27e6ea65fc03978407195ef79570be24a
Omar Gonzalez <omargonzalez@iMac-de-Omar.local>
2025-04-14 14:26:55 -0500

    Se adjunta cambio de url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
         siteKey: '6Lc8phgrAAAAAOdp4qjIgyRf6x-bt20Hutvm4BFw',
     },
     //urlApi: 'http://187.175.225.67:8028/api'
-    urlApi: 'https://tramitesyservicios.gobiernodesolidaridad.gob.mx/service/api'
+    urlApi: 'https://tramitesyservicios.playadelcarmen.gob.mx/service/api'
 };
